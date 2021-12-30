d755d54dcc45c80c1bae682942dc89ac4503d0d8
梶田英完 <hidemasa_kajita@respawn.jp>
2021-12-30 13:18:28 +0900

    rename

--- src/input/user/CreateUserInput.ts
+++ src/input/user/CreateUserInput.ts
@@ -2,7 +2,7 @@ import { InputType, Field } from 'type-graphql'
 import { User } from '../../entity/User'
 
 @InputType({ description: 'New User Argument' })
-export class AddUserInput implements Partial<User> {
+export class CreateUserInput implements Partial<User> {
   @Field()
   firstName?: string
   @Field()
--- src/resolver/user/UserMutationResolver.ts
+++ src/resolver/user/UserMutationResolver.ts
@@ -2,12 +2,12 @@ import { Resolver, Arg, Ctx, Mutation } from 'type-graphql'
 import { User } from '../../entity/User'
 import { Context } from 'apollo-server-core'
 import { getRepository } from 'typeorm'
-import { AddUserInput } from '../../input/user/AddUserInput'
+import { CreateUserInput } from '../../input/user/CreateUserInput'
 
 @Resolver()
 export class UserMutationResolver {
   @Mutation((returns) => User)
-  async addUser(@Arg('data') newUser: AddUserInput, @Ctx() ctx: Context): Promise<User> {
+  async addUser(@Arg('data') newUser: CreateUserInput, @Ctx() ctx: Context): Promise<User> {
     const userRepository = getRepository(User)
 
     return await userRepository.save(newUser)
